aecbaee8d140887b993be11a1e5ed2bdc8e6ae59
HamzaGbada <hamza.gbada@gmail.com>
2023-11-19 21:02:52 +0100

    [INIT] controller

--- src/main/java/com/gte3/mongodemo/MongoDemoApplication.java
+++ src/main/java/com/gte3/mongodemo/MongoDemoApplication.java
@@ -2,8 +2,10 @@ package com.gte3.mongodemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories
 class MongoDemoApplication {
 
     public static void main(String[] args) {
