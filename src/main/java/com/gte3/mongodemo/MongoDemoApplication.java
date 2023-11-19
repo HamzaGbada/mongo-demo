76fa96bd6f8b1b4859e57769e76c3b4d49c8e2f4
HamzaGbada <hamza.gbada@gmail.com>
2023-11-19 20:27:04 +0100

    [INIT] document

--- src/main/java/com/gte3/mongodemo/MongoDemoApplication.java
+++ src/main/java/com/gte3/mongodemo/MongoDemoApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MongoDemoApplication {
+class MongoDemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MongoDemoApplication.class, args);
